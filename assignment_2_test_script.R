260312f51075274451a4f11fe688a67aba163dfa
Michael Kinneen <michaelkinneen@.com>
2023-01-20 12:06:20 -0800

    updated test script

--- assignment_2_test_script.R
+++ assignment_2_test_script.R
@@ -1,6 +1,11 @@
 ###Testing script for creating new branch
 
+thesis_work<-"Good"
 print("How is the craic")
 
-#resposne
-print("The craic is mighty")
+#response
+ifelse(thesis_work=="Good"){
+  print("The craic is mighty")
+}else {
+  print("No craic")
+}
